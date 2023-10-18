1e8b836e26c1b091040169ee8c257f9fa8b2f7a3
Neil Benavides <nbenavides07@gmailcom>
2023-10-18 15:25:46 -0500

    Proyecto Integrador parte 2

---
+++ proyecto_integrador2.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datasets import load_dataset
+
+dataset = load_dataset("mstz/heart_failure")
+data = dataset["train"]
+
+# Pregunta 1
+df = pd.DataFrame(data)
+print(df)
+
+# Pregunta 2
+df_personas_perecieron = df[df['is_dead'] == 1]['is_dead']
+print(df_personas_perecieron)
+
+df_personas_no_perecieron = df[df['is_dead'] == 0]['is_dead']
+print(df_personas_no_perecieron)
+
+# Pregunta 3
+promedio_1 = df[df['is_dead'] == 1]['age'].mean()
+print(promedio_1)
+
+promedio_2 = df[df['is_dead'] == 0]['age'].mean()
+print(promedio_2)
