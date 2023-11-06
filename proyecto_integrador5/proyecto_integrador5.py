b14182f893d18d22ba0fb835758fa05b68031d1f
Neil Benavides <nbenavides07@gmailcom>
2023-11-06 10:19:01 -0500

    Proyecto Integrador parte 5

---
+++ proyecto_integrador5/proyecto_integrador5.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import requests
+
+def procesar_datos(url, columnas, bins, labels, nombre_archivo):
+    # Descargar los datos
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open('heart_failure_clinical_records_dataset.csv', 'w') as file:
+            file.write(response.text)
+    df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+
+    # Verificar valores faltantes
+    if df.isnull().any().any():
+        print("Hay valores faltantes en el DataFrame.")
+    else:
+        print("No hay valores faltantes en el DataFrame.")
+
+    # Verificar filas duplicadas
+    if df.duplicated().any():
+        print("Hay filas duplicadas en el DataFrame.")
+    else:
+        print("No hay filas duplicadas en el DataFrame.")
+
+    # Eliminar valores atípicos
+    for columna in columnas:
+        Q1 = df[columna].quantile(0.25)
+        Q3 = df[columna].quantile(0.75)
+        IQR = Q3 - Q1
+        filtro = (df[columna] >= Q1 - 1.5 * IQR) & (df[columna] <= Q3 + 1.5 *IQR)
+        df = df.loc[filtro]
+
+    # Categorizar edades
+    df['categoria_edad'] = pd.cut(df['age'], bins=bins, labels=labels)
+
+    # Guardar los datos
+    df.to_csv(nombre_archivo, index = False)
+    print(df)
+
+# Uso de la función
+url = 'https://huggingface.co/datasets/mstz/heart_failure/raw/main/heart_failure_clinical_records_dataset.csv'
+columnas = ['creatinine_phosphokinase', 'ejection_fraction', 'platelets', 'serum_sodium']
+bins = [0, 12, 19, 39, 59, np.inf]
+labels = ['Niño', 'Adolescente', 'Jóvenes adulto', 'Adulto', 'Adulto mayor']
+
+procesar_datos(url, columnas, bins, labels, 'Datos_procesados.csv')
